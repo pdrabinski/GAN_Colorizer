8943b2c788c55d3b738388fc366cf5e1dc3415d9
pdrabinski <pdrabinski@gmail.com>
2018-01-17 17:36:20 -0700

    fixed grayscaling

--- src/grayscale_cifar10.py
+++ src/grayscale_cifar10.py
@@ -13,21 +13,22 @@ def iter_over_image(image):
     """
     Iterate over each pixel in image, pass each pixel to grayscale function
     """
+    image_copy = np.ones([image.shape[0],image.shape[1]])
     for i in range(len(image)):
-        for j,pixel in enumerate(image[i]):
-            image[i][j] = grayscale(pixel)
-    return image
+        for j in range(len(image[i])):
+            image_copy[i][j] = grayscale(image[i][j])
+    return image_copy
 
 
 if __name__ == '__main__':
     (X_train, y_train), (X_test, y_test) = cifar10.load_data()
     #8 is for ships
-    X_train = [X_train[i] for i in range(len(y_train)) if y_train[i] == 8]
-    X_test = [X_test[i] for i in range(len(y_test)) if y_test[i] == 8]
+    X_train = np.array([X_train[i] for i in range(len(y_train)) if y_train[i] == 8])
+    X_test = np.array([X_test[i] for i in range(len(y_test)) if y_test[i] == 8])
 
-    X_train_true = np.array(X_train)
-    # y_train_img = Image.fromarray(y_train[0],'RGB')
-    # y_train_img.show()
+    X_train_true = X_train
+    # X_train_true_img = Image.fromarray(X_train_true[0],'RGB')
+    # X_train_true_img.show()
     with open('../data/X_train_true.p','wb') as f:
         pickle.dump(X_train_true,f)
     print('X_train_true done...')
@@ -40,8 +41,8 @@ if __name__ == '__main__':
     print('X_train done...')
 
     X_test_true = np.array(X_test)
-    # y_test_img = Image.fromarray(y_test[0],'RGB')
-    # y_test_img.show()
+    # X_test_true_img = Image.fromarray(X_test_true[0],'RGB')
+    # X_test_true_img.show()
     with open('../data/X_test_true.p','wb') as f:
         pickle.dump(X_test_true,f)
     print('X_test_true done...')
