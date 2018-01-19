bc84477c143ad31032383d3f58163b8533659908
pdrabinski <pdrabinski@gmail.com>
2018-01-18 23:00:33 -0700

    strides of 2

--- src/gan.py
+++ src/gan.py
@@ -72,7 +72,7 @@ class Discriminator():
     def build(self, input_shape):
         self.d_input = Input(shape=input_shape)
 
-        self.model = Conv2D(64, (3, 3), padding='same', stride=2)(self.d_input)
+        self.model = Conv2D(64, (3, 3), padding='same', strides=2)(self.d_input)
         self.model = LeakyReLU(.2)(self.model)
         self.model = Dropout(.25)(self.model)
 
