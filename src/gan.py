71b0223e315f6ed18f64324e94bcc4ff7034a1d3
pdrabinski <pdrabinski@gmail.com>
2018-01-18 23:27:32 -0700

    max pooling

--- src/gan.py
+++ src/gan.py
@@ -73,11 +73,13 @@ class Discriminator():
         self.d_input = Input(shape=input_shape)
 
         self.model = Conv2D(32, (3, 3), padding='same', activation='relu')(self.d_input)
-        self.model = Conv2D(32, (3, 3), padding='same', activation='relu', strides=2)(self.model)
+        self.model = Conv2D(32, (3, 3), padding='same', activation='relu')(self.model)
+        self.model = MaxPooling2D(pool_size=(2, 2))(self.model)
         self.model = Dropout(.25)(self.model)
 
         self.model = Conv2D(64, (3, 3), padding='same', activation='relu')(self.model)
-        self.model = Conv2D(64, (3, 3), padding='same', activation='relu', strides=2)(self.model)
+        self.model = Conv2D(64, (3, 3), padding='same', activation='relu')(self.model)
+        self.model = MaxPooling2D(pool_size=(2, 2))(self.model)
         self.model = Dropout(.25)(self.model)
 
         self.model = Flatten()(self.model)
