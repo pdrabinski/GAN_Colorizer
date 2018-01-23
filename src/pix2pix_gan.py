9d6b69a35a6b0823b1d3121847dc9907e1377bc0
pdrabinski <pdrabinski@gmail.com>
2018-01-22 17:07:50 -0700

    update params

--- src/pix2pix_gan.py
+++ src/pix2pix_gan.py
@@ -213,8 +213,8 @@ if __name__ == '__main__':
     X_test_AB = X_test_AB.astype('float32')
     print('X_test done...')
 
-    batch_epochs = 500
-    batch_size = 64
+    batch_epochs = 100
+    batch_size = 128
 
     gan = GAN()
     gan.train(X_train_L, X_train_AB, X_test_L, X_test_AB, batch_epochs, batch_size)
