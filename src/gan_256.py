5b3dce454dc7a21865cb7846f37bff90c3aec176
pdrabinski <pdrabinski@gmail.com>
2018-01-24 16:31:45 -0700

    increased dropout

--- src/gan_256.py
+++ src/gan_256.py
@@ -120,7 +120,7 @@ class GAN():
         model = Sequential()
         model.add(Conv2D(32, (3, 3), padding='same', input_shape=self.d_input_shape, strides=2))
         model.add(LeakyReLU(.2))
-        model.add(Dropout(.25))
+        # model.add(Dropout(.25))
 
         model.add(Conv2D(64, (3, 3), padding='same',strides=2))
         model.add(BatchNormalization())
@@ -135,7 +135,7 @@ class GAN():
         model.add(Conv2D(256, (3, 3), padding='same',strides=2))
         model.add(BatchNormalization())
         model.add(LeakyReLU(.2))
-        model.add(Dropout(.25))
+        model.add(Dropout(.5))
 
         # model.add(Conv2D(512, (3, 3), padding='same',strides=2))
         # model.add(BatchNormalization())
