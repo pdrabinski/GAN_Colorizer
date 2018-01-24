5f72846087a6a302f9cbb4a3b265bfb3b1a8e340
pdrabinski <pdrabinski@gmail.com>
2018-01-24 11:34:17 -0700

    adjust training algorithm

--- src/pix2pix_gan_full_batches.py
+++ src/pix2pix_gan_full_batches.py
@@ -152,7 +152,7 @@ class GAN():
             d_loss = self.discriminator.fit(x=X_train_AB,y=y_train_real,batch_size=32,epochs=1)
             if e % 15 == 14:
                 noise = np.random.rand(n,32,32,2) * 2 -1
-                d_loss = self.discriminator.fit(x=noise,y=y_train_fake, batch_size=32)
+                d_loss = self.discriminator.fit(x=noise,y=y_train_fake, batch_size=32, epochs=1)
             d_loss = self.discriminator.fit(x=generated_images,y=y_train_fake,batch_size=32,epochs=1)
             d_losses.append(d_loss.history['loss'][-1])
             d_acc.append(d_loss.history['acc'][-1])
@@ -167,7 +167,7 @@ class GAN():
             g_losses.append(g_loss.history['loss'][-1])
             print('Generator Loss: ', g_loss.history['loss'][-1])
             disc_acc = d_loss.history['acc'][-1]
-            if disc_acc < .8:
+            if disc_acc < .8 and e < 5:
                 self.pre_train_discriminator(X_train_L, X_train_AB, X_test_L, X_test_AB)
             if e % 5 == 4:
                 print(e + 1,"batches done")
